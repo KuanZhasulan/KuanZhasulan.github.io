9b0cb8b817653e40a7f15d537a739c20c90c6885
Kuan Zhassulan <kuanzhasulan@gmail.com>
2020-03-21 01:52:12 +0500

    Updates

---
+++ precache-manifest.066b6d293d8d71d8c76d77e587320dc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b9f60fa3bec8e847fdc4d3033c519cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75dfe9793ca9add995f2",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d36dd768fd3d305c595a",
+    "url": "/static/js/2.a3e335fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a3e335fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75dfe9793ca9add995f2",
+    "url": "/static/js/main.cefb21b8.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "cba429d9358689b7c8d187ea54cef76f",
+    "url": "/static/media/profile.cba429d9.jpg"
+  }
+]);
